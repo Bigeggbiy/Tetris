9ddb91802cc266f07dbaae88df7fa3d67c55ad85
Cole Powell <cjpowell517@gmail.com>
2024-10-18 12:20:43 -0600

    collision detection
    Added collision to square object it will detect when it collides with an object that has the "Tetromino" tag

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Square : MonoBehaviour
 {
+    public bool hasPiece;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasPiece = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // Check if the colliding object has the "Tetromino" tag
+        if (collision.gameObject.CompareTag("Tetromino"))
+        {
+            hasPiece = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // When the Tetromino leaves, reset the state
+        if (collision.gameObject.CompareTag("Tetromino"))
+        {
+            hasPiece = false;
+        }
     }
 }
+
