4474c6458632fa49dd767ca438849120f377e545
MattHarris <mharrisdigital@gmail.com>
2024-11-19 21:13:17 -0700

    Removed unused code

--- Assets/Scripts/UI Scripts/StartMenuController.cs
+++ Assets/Scripts/UI Scripts/StartMenuController.cs
@@ -5,12 +5,6 @@ public class StartMenuController : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Main scene"); // Replace with your game scene name
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Game Quit!"); // Only works in a built application
+        SceneManager.LoadScene("Main scene");
     }
 }
